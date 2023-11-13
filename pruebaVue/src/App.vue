0398bc45931867c32ae2477562fc508e441c01ea
nando008 <josefdorobles0801@gmail.com>
2023-11-13 17:44:10 -0500

    act

--- pruebaVue/src/App.vue
+++ pruebaVue/src/App.vue
@@ -1,13 +1,33 @@
 <script setup>
   let saluda = "hello world";
-
-
 </script>
 
 <template>
   <h1>{{ saluda }}</h1>
+
+  <div>
+    <h2>-</h2>
+    <h2>+</h2>
+  </div>
+
 </template>
 
 <style scoped>
+
+ h1{
+  background-color: aqua;
+  text-align: center;
+ }
+ div{ 
+  display: flex;
+  justify-content: center;
+ }
+
+ h2{
+  background-color: blue;
+  width: 30px;
+  text-align: center;
+  margin: 0px 10px;
+ }
 
 </style>
